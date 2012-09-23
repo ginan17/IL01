71d4f677e31d44dd61fff7b4031cf0275d6f48f9
Ginan <ginanjar.pramadita@sangkuriang.co.id>
2012-09-24 00:24:58 +0700

    Perbaiki backing, template, dan css

---
+++ src/InLanceWeb/src/java/com/inlance/web/backing/FreelancerBrowserBacking.java
@@ -0,0 +1,14 @@
+package com.inlance.web.backing;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Ginan
+ */
+@ManagedBean
+@ViewScoped
+public class FreelancerBrowserBacking extends AbstractBacking {
+	
+}
